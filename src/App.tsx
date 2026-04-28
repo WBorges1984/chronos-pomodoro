b25e8eb3fd638772c21e8d11570549f0e3c6eee0
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2026-04-27 21:00:39 -0300

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { Home } from "./Pages/Home";
 import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  const [state, setIsHome] = useState(true);
+  const [state, setState] = useState(initialState);
 
-  return <Home />;
+  return <Home state={state} setState={setState} />;
 }
